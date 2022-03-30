be9099a646e75b03a5300ac898eb2e71b4401890
rosario-garufi <saro.garufi1@gmail.com>
2022-03-30 16:09:49 +0200

    snack7 consegna

--- asset/javascript/snack7/snack7.js
+++ asset/javascript/snack7/snack7.js
@@ -2,8 +2,10 @@
 Stampa le potenze di 2 fino a 1000
 */ 
 
-for(let i = 0; i<1000; i++){
     
-    let potenza = Math.pow(2, i);
+for(let i = 0; Math.pow(2, i)<1000; i++){
+    let potenza = Math.pow(2,i);
     console.log(potenza);
-}
+    
+    
+    }
